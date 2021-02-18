ec93f79e7ae16db45380925ae4cb4b3896130f1b
Personal <personal@Daniels-MacBook-Pro.local>
2021-02-18 22:21:36 +0000

    remove comments

--- src/accountTypeChecker/accountTypeChecker.spec.ts
+++ src/accountTypeChecker/accountTypeChecker.spec.ts
@@ -3,23 +3,11 @@ import {
   mockVaryingAccountBalanceHistory,
   mockConsistentAccountBalanceHistory,
 } from './accountTypeChecker.data';
-/*
-
-GIVEN I want to track a users spending habits 
-WHEN it goes down by varying amounts each month
-THEN it should return A
-
-GIVEN I want to track a users spending habits 
-WHEN it goes down by the same amount each month
-THEN it should return B
-
-accepts multiple inputs for same month
-
-*/
 
 test('determines if the balance goes down by varying amounts each month', () => {
   expect(accountTypeChecker(mockVaryingAccountBalanceHistory)).toEqual('A');
 });
+
 test('determines if the balance goes down by the same amount each month', () => {
   expect(accountTypeChecker(mockConsistentAccountBalanceHistory)).toEqual('B');
 });
