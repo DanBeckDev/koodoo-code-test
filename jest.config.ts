68754925337fae0ee3d5762f916ab1cbaa00233e
Personal <personal@Daniels-MacBook-Pro.local>
2021-02-19 07:46:51 +0000

    add jest code coverage thresholds

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,13 @@
 export default {
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html'],
-  roots: ['src'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+    roots: ['src'],
 };
